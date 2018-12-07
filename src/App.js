62e3e136ef3aa3cd01689c7b6b9eb205fbd20536
sakhayadeep <sakhayadeepnath@gmail.com>
2018-12-07 13:56:15 +0530

    complete v1.1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,59 @@ import React, { Component } from "react";
 import "./App.css";
 import Image from "./components/image";
 import Navbar from "./components/navbar";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    images: []
+  };
+
+  uploadFile = files => {
+    // Push all the axios request promise into a single array
+    const uploaders = files.map((file, id) => {
+      // Initial FormData
+      const formData = new FormData();
+      formData.append("file", file);
+      formData.append("tags", `codeinfuse, medium, gist`);
+      formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
+      formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
+      formData.append("timestamp", (Date.now() / 1000) | 0);
+
+      const url =
+        "https://api.cloudinary.com/v1_1/" +
+        process.env.REACT_APP_CLOUDINARY_CLOUD_NAME +
+        "/image/upload";
+
+      // Make an AJAX upload request using Axios
+      return axios
+        .post(url, formData, {
+          headers: { "X-Requested-With": "XMLHttpRequest" }
+        })
+        .then(response => {
+          const data = response.data;
+          const fileURL = data.secure_url; //should store this URL for future references the app
+          console.log(data);
+
+          //adding into the state
+          let updatedImages = [...this.state.images];
+          updatedImages.push(fileURL);
+          this.setState({ images: updatedImages });
+        });
+    });
+
+    // Once all the files are uploaded
+    axios.all(uploaders).then(() => {
+      // ... perform after upload is successful operation
+    });
+  };
+
+  removeImageHandler = event => {
+    event.preventDefault();
+    let updatedImages = [...this.state.images];
+    updatedImages.splice(event.target.id, 1);
+    this.setState({ images: updatedImages });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -11,7 +62,11 @@ class App extends Component {
         <hr />
         <main className="container">
           <div className="jumbotron">
-            <Image />
+            <Image
+              images={this.state.images}
+              onUpload={this.uploadFile}
+              onRemove={this.removeImageHandler}
+            />
           </div>
         </main>
       </React.Fragment>
--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,60 +1,9 @@
 import React, { Component } from "react";
 import Dropzone from "react-dropzone";
-import axios from "axios";
 
 class Image extends Component {
-  state = {
-    images: []
-  };
-
-  uploadFile = files => {
-    // Push all the axios request promise into a single array
-    const uploaders = files.map((file, id) => {
-      // Initial FormData
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("tags", `codeinfuse, medium, gist`);
-      formData.append("upload_preset", process.env.REACT_APP_CLOUDINARY_PRESET);
-      formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
-      formData.append("timestamp", (Date.now() / 1000) | 0);
-
-      const url =
-        "https://api.cloudinary.com/v1_1/" +
-        process.env.REACT_APP_CLOUDINARY_CLOUD_NAME +
-        "/image/upload";
-
-      // Make an AJAX upload request using Axios
-      return axios
-        .post(url, formData, {
-          headers: { "X-Requested-With": "XMLHttpRequest" }
-        })
-        .then(response => {
-          const data = response.data;
-          const fileURL = data.secure_url; //should store this URL for future references the app
-          console.log(data);
-
-          //adding into the state
-          let updatedImages = [...this.state.images];
-          updatedImages.push(fileURL);
-          this.setState({ images: updatedImages });
-        });
-    });
-
-    // Once all the files are uploaded
-    axios.all(uploaders).then(() => {
-      // ... perform after upload is successful operation
-    });
-  };
-
-  removeImageHandler = event => {
-    event.preventDefault();
-    let updatedImages = [...this.state.images];
-    updatedImages.splice(event.target.id, 1);
-    this.setState({ images: updatedImages });
-  };
-
   render() {
-    const images = this.state.images.map((imgSrc, i) => {
+    const images = this.props.images.map((imgSrc, i) => {
       return (
         <div className="badge">
           <img style={{ width: 100 }} src={imgSrc} />
@@ -62,7 +11,7 @@ class Image extends Component {
           <button
             className="btn btn-danger m-2"
             id={i}
-            onClick={this.removeImageHandler.bind(this)}
+            onClick={this.props.onRemove.bind(this)}
             href="#"
           >
             Remove image
@@ -73,7 +22,7 @@ class Image extends Component {
     return (
       <div className="d-flex flex-row ">
         <div className="col-4 p-2">
-          <Dropzone onDrop={this.uploadFile} multiple accept="image/*">
+          <Dropzone onDrop={this.props.onUpload} multiple accept="image/*">
             <p className="text-center">Drop your files here</p>
           </Dropzone>
         </div>
